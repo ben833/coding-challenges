8eabe55e8894efb0362d61c01d67db998edbaa70
Ben Margolis <ben833@gmail.com>
2013-06-09 09:43:45 -0400

    Fixed incorrect answers for numbers like seventy three hundred twenty four thousand

--- NumberWords/NumberWords.php
+++ NumberWords/NumberWords.php
@@ -71,31 +71,64 @@ class NumberWords {
     
     $words = explode(" ", $number);
     //print_r($words);
-    $last_word = "";
+    $previous_words = array();
+    $current_multiplier = "";
     
     // go through all the number words in the phrase and construct the answer in numerical form
     foreach ($words as $word) {
       if ($word == "negative") {
         $isPositive = false;
       } else {
-        //echo "*" . $word;
         // if we're seeing a big number word, make modifications to the number we think we have
+        // consider that there might be more than just one number prior to seeing the big number word: thirty three thousand
+        // so go back till you hit a big number word or beginning of the phrase and subtract what you saw so far and multiply by
+        // the big number word
         if (in_array($word, array("hundred", "thousand", "million"))) {
-          // debug // echo "<br>big number: " . $word . " and I'm at: " . $return_value . "<br>";
-          
-          // the previous word we saw is actually much bigger, so forget that we've even seen it
-          $return_value -= $translate[$last_word];
+          $current_multiplier = 0;
+          $saw_hundred = false;
+
+          // Take care of numbers like four million eight hundred forty nine thousand
+          // Having a "hundred" is a special case because you need to multiply that by the word that comes before it 
+          // and then add what's after it. This loop will run at the worst case four times. 
+          foreach ($previous_words as $previous_word) {
+            // Remember words all the way back to the beginning of the word or the last time you saw a "million" or "thousand"
+            // because at that point you don't need to worry about multiplying by 100
+            if (in_array($previous_word, array("thousand", "million"))) {
+              break;
+            } else { // we are looking at a "hundreds" segment of a number
+              if ($previous_word == "hundred") {
+                // looking back, we realize we are dealing with a number in the hundreds
+                // now we need to look back further to see how many hundreds we are dealing with
+                $saw_hundred = true;
+              } else {
+                if ($saw_hundred) {
+                  // we just found out how many hundreds we are dealing with, so modify the number segment
+                  $return_value -= 100*$translate[$previous_word];
+                  $current_multiplier += 100*$translate[$previous_word];
+                } else {
+                  // dealing with numbers in the ones and tens places of the segment of the number
+                  // simply subtract what we saw from what we think the number is, and remember this number
+                  // to use it when we figure out the final value of this segment of the number
+                  $return_value -= $translate[$previous_word];
+                  $current_multiplier += $translate[$previous_word];
+                }
+                // reset the fact that we saw a hundred in this segment of the number
+                $saw_hundred = false;
+              }
+              // debug // echo "current_multiplier: " . $current_multiplier . "<br>";
+            }
+          }
           
           // multiply the big number by the number we saw last to get the full value of the clause we're seeing
-          $return_value += $translate[$word] * $translate[$last_word]; // need to just multiply the last number, not the whole return_value
+          $return_value += $translate[$word] * $current_multiplier; // multiply the "hundreds" segment of the number
           
-          // debug // echo "big number: " . $word . " and I'm now at: " . $return_value . " because last_word is: " . $translate[$last_word] . "<br>";
+          // debug //echo "big number: " . $word . " and I'm now at: " . $return_value . " because current_multiplier is: " . $current_multiplier . "<br>";
         } else {
           // add the number that we just saw to the answer
           $return_value += $translate[$word];
         }
       }
-      $last_word = $word;
+      array_unshift($previous_words, $word);
     }
     
     // make the whole thing negative if the first word was negative
--- NumberWords/NumberWordsTest.php
+++ NumberWords/NumberWordsTest.php
@@ -15,21 +15,26 @@ $tests = array (
   "six" => 6,
   "negative seven hundred twenty nine" => -729, 
   "one million one hundred one" => 1000101,
+  "thirty three million three hundred thirty three thousand three hundred thirty three" => 33333333,
+  "thirty three thousand" => 33000,
+  "three hundred thirty three thousand" => 333000,
+  "three hundred thirty three thousand three" => 333003,
+  "four hundred million eight hundred one thousand four" => 400801004,
 
 );
 
 $numberz = new NumberWords();
 $result = "";
+echo "<table border='1'>";
 foreach ($tests as $test => $value) {
   if ($numberz->translate_phrase($test) == $value) {
     $result = "PASS";
   } else {
     $result = "FAIL " . $numberz->translate_phrase($test);
   }
-  echo "<br>" . $test . " = " . $value . ": " . $result;
-  
+  echo "<tr><td>" . $test . " = " . $value . "</td><td>" . $result . "</td></tr>";
 }
-
+echo "</table>";
 
 
 ?>
